488b4d18108855fe2cd7b8d0dcd998b8ca164eaf
vickyporras <porrasortizvictoria@gmail.com>
2022-04-19 10:32:50 +0200

    probamos con ejemplos el get y el set

--- persona/Main.java
+++ persona/Main.java
@@ -2,6 +2,10 @@ public class Main{
   public static void main (String[] args){
     Persona miPersona = new Persona("Paula", "9875640K");
     System.out.println(miPersona);
-
+    System.out.println("el nombre es " + miPersona.getNombre());
+    System.out.println("el dni es " + miPersona.getDNI());
+    miPersona.setNombre("celia");
+    miPersona.setDNI("17263m");
+    System.out.println(miPersona);
   }
 }
