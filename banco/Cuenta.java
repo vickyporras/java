fc0e7709766db5930a0b8d561101f3e06bebedf5
vickyporras <porrasortizvictoria@gmail.com>
2022-04-21 08:59:20 +0200

    empiezo a crear la clase cuenta

---
+++ banco/Cuenta.java
@@ -0,0 +1,36 @@
+public class Cuenta{
+  protected String titular;
+  protected Double cantidad;
+
+  //constructores
+  public Cuenta(){
+    this.titular="Victoria";
+    this.cantidad=280.000;
+  }
+  public Cuenta(String titular, Double cantidad){
+    this.titular=titular;
+    this.cantidad=cantidad;
+  }
+  public Cuenta(Cuenta cuenta){
+    this.titular = cuenta.titular;
+    this.cantidad = cuenta.cuenta;
+  }
+  public String getTitular(){
+    return this.titular;
+  }
+  public Double getCantidad{
+    return this.cantidad;
+  }
+  public void setTitular(String titular){
+   this.titular = titular;
+  }
+  public void setCantidad(Double cantidad){
+   this.cantidad = cantidad;
+  }
+
+
+  //ToString
+  public String toString(){
+    return "El titular de la cuenta es  " + this.titular + ", y la cantidad de su cuenta es " + this.cantidad;
+  }
+}
