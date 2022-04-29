f3da8b94882d601937f3c4ec565e601c1b061f7b
vickyporras <porrasortizvictoria@gmail.com>
2022-04-29 08:36:00 +0200

    y

--- electrodomesticos_rafactorizado/Main.java
+++ electrodomesticos_rafactorizado/Main.java
@@ -2,5 +2,6 @@ class Main {
   public static void main(String[] args){
     Electrodomestico miElectrodomestico = new Electrodomestico(120,"miColor","miConsumo",120);
     System.out.println(miElectrodomestico);
+    System.out.println();
   }
 }
