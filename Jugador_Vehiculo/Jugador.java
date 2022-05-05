89ce0f7947d21b773e2b0ace72f04d3394d4c046
vickyporras <porrasortizvictoria@gmail.com>
2022-05-05 09:06:31 +0200

    clase jugador

---
+++ Jugador_Vehiculo/Jugador.java
@@ -0,0 +1,11 @@
+
+public class Jugador{
+  protected String nombre;
+  protected int vidas;
+  protected int[] coordenadas;
+
+  private final int VIDAS = 3;
+  public Jugador(String nombre){
+    
+  }
+}
