636b14ef50bcd968c4903c1f7a8da5a384be7a0b
vickyporras <porrasortizvictoria@gmail.com>
2022-05-09 13:19:44 +0200

    okr

--- contraseña/ContraseniaNum.java
+++ contraseña/ContraseniaNum.java
@@ -1,6 +1,6 @@
 public class ContraseniaNum extends Contrasenia{
-  public  String caratValido(){
-    return "no esta hecha";
+  public  String caratValido(String contraseniaPrueba){
+    return "no hay manera";
   }
   public  String tamaValido(){
     return "no esta hecha";
