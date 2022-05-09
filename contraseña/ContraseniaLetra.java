4b65a9dafa7e6f8c194815ddfc927b8e889474d3
vickyporras <porrasortizvictoria@gmail.com>
2022-05-09 18:26:12 +0200

    crera contraseñaLetra

---
+++ contraseña/ContraseniaLetra.java
@@ -0,0 +1,8 @@
+public class ContraseniaLetra extends Contrasenia{
+  public  String caratValido(String contraseniaPrueba){
+    return "no hay manera";
+  }
+  public  String tamaValido(){
+    return "no esta hecha";
+  }
+}
